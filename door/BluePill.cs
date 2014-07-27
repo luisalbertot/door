aa9965cee17e095177c2660b184d4e92b1b671df
luisalbertot@gmail.com <luisalbertot@gmail.com>
2014-07-27 19:16:30 +1000

    Initial version

---
+++ door/BluePill.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using knockknock.readify.net;
+
+namespace door
+{
+    public static class BluePill
+    {
+
+        public static ContactDetails WhoAreYou()
+        {
+            return new ContactDetails()
+            {
+                EmailAddress = "luisalbertot@gmail.com",
+                GivenName = "Luis Alberto",
+                FamilyName = "Tchakerian",
+                PhoneNumber = "0402261117"
+            };
+        }
+
+        public static long Fibonnaci(long n)
+        {
+            if (n == 0 || n == 1)
+            {
+                return n;
+            }
+            else
+            {
+                /*
+                    //F_n = F_{n-1} + F_{n-2},
+                    return fbn(n - 1) + fbn(n - 2);
+                }*/
+
+                //Flat
+                long i, fib0, fib1, nAbs = Math.Abs(n);
+                fib0 = 0;
+                fib1 = 1;
+                for (i = 1; i <= nAbs; i++)
+                {
+                    fib0 += fib1;
+                    fib1 = fib0 - fib1;
+                }
+
+                if (n < 0 && (!(n % 2 == 1))) { fib0 = -fib0; }
+                return fib0;
+
+            }
+        }
+        public static double FibonnaciAprox(long n)
+        {
+            //Phi20/sqrt(5)
+            double phi = 1.618;
+            long nAbs = Math.Abs(n);
+            double aprox = Math.Pow(phi, nAbs) / Math.Sqrt(5);
+            if (n < 0 && (!(n % 2 == 1))) { aprox = -aprox; }
+            return aprox;
+        }
+
+        public static TriangleType WhatShapeIsThis(int a, int b, int c)
+        {
+            TriangleType triangleType = TriangleType.Error;
+
+            long la = a, lb = b, lc = c;
+
+            if (
+                (la > 0)
+                && (lb > 0)
+                && (lc > 0)
+
+                && (la + lb > lc)
+                && (lb + lc > la)
+                && (lc + la > lb)
+                )
+            {
+                //OK
+                triangleType = TriangleType.Scalene;
+                if (a == b && b == c)
+                {
+                    triangleType = TriangleType.Equilateral;
+                }
+                else if (a == b || b == c || c == a)
+                {
+                    triangleType = TriangleType.Isosceles;
+                }
+            }
+
+            return triangleType;
+        }
+
+        public static string ReverseWords(string s)
+        {
+            List<string> reversedWords = new List<string>();
+            foreach (string word in s.Split(' '))
+            {
+                reversedWords.Add(invertString(word));
+            }
+            return String.Join(" ", reversedWords.ToArray());;
+        }
+
+        private static string invertString(string word)
+        {
+            string drow = "";
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+                drow += word.Substring(i, 1);
+            }
+            return drow;
+        }
+
+    }
+}
---
+++ door/RedPill.svc.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Web;
+using knockknock.readify.net;
+
+namespace door
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "RedPill" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select RedPill.svc or RedPill.svc.cs at the Solution Explorer and start debugging.
+    public class RedPill : IRedPill
+    {
+        public ContactDetails WhoAreYou()
+        {
+            log(DateTime.Now + " - WhoAreYou();" + Environment.NewLine);
+            return BluePill.WhoAreYou();
+        }
+
+        public long FibonacciNumber(long n)
+        {
+            log(DateTime.Now + " - FibonacciNumber(" + n + ")=");
+
+            long fib;
+            try
+            {
+                Convert.ToInt64(BluePill.FibonnaciAprox(n));
+            }
+            catch(System.OverflowException)
+            {
+                log("ArgumentOutOfRangeException;" + Environment.NewLine);
+                throw new System.ServiceModel.FaultException<System.ArgumentOutOfRangeException>(new System.ArgumentOutOfRangeException("Fibonacci result will be 'too long'"), new FaultReason("Fibonacci result will be 'too long'"));
+            }
+            fib = BluePill.Fibonnaci(n);
+            log(fib + ";" + Environment.NewLine);
+            return fib;
+        }
+
+
+
+        public TriangleType WhatShapeIsThis(int a, int b, int c)
+        {
+            log(DateTime.Now + " - WhatShapeIsThis(" + a + "," + b + "," + c + ")=");
+            TriangleType triangleType = BluePill.WhatShapeIsThis(a, b, c);
+            log(triangleType + ";" + Environment.NewLine);
+            return triangleType;
+        }
+
+        public string ReverseWords(string s)
+        {
+            log(DateTime.Now + " - ReverseWords(\"" + s + "\")=");
+            string result = null;
+            if (s != null)
+            {
+                result = BluePill.ReverseWords(s);
+                log("\"" + result + "\";" + Environment.NewLine);
+            }
+            else
+            {
+                log("ArgumentNullException;" + Environment.NewLine);
+                throw new System.ServiceModel.FaultException<System.ArgumentNullException>(new System.ArgumentNullException("Parameter can't be null"), new FaultReason("Parameter can't be null"));
+            }
+            return result;
+        }
+
+        private void log(string text)
+        {
+            File.AppendAllText(HttpContext.Current.Server.MapPath("~/App_Data/log.txt"), text);
+        }
+
+    }
+}
